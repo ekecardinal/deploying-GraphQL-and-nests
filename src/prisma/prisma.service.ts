7747acf28a190f7ec573b30d0252c826f03a5209
ekecardinal <ekecardinal@gmail.com>
2024-09-13 11:27:49 +0100

    adjusted deploy

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,9 +6,9 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
-  async enableShutdownHooks(app: INestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
-  }
+  // async enableShutdownHooks(app: INestApplication) {
+  //   this.$on('beforeExit', async () => {
+  //     await app.close();
+  //   });
+  // }
 }
